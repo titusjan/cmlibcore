306d78b60fac0341257c6f7e72342e08ad464805
Pepijn Kenter <titusjan@gmail.com>
2019-02-17 15:00:02 +0100

    Fixed typo.

--- cmlibcore/__init__.py
+++ cmlibcore/__init__.py
@@ -14,7 +14,7 @@
 
 from .misc import __version__
 
-# Classes that may be of user externally. I.e. by users of CmLib
+# Classes that may be of use externally. I.e. by users of CmLib
 from .cmap import ColorLib, ColorMap, DataCategory
 from .qtwidgets.browser import CmLibBrowserDialog
 from .qtwidgets.selection import ColorSelectionWidget
